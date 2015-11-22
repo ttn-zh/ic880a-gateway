c20957a5ff80a86b9795ff43a0a98516c9a0ebf1
Gonzalo Casas <gonzalo.casas@local.ch>
2015-11-22 15:26:24 +0100

    Self exec if changed

--- install.sh
+++ install.sh
@@ -9,8 +9,13 @@ if [[ $1 != "" ]]; then VERSION=$1; fi
 # Update the gateway installer to the correct branch (defaults to master)
 echo "Updating installer files..."
 git checkout -q $VERSION
+OLD_HEAD=$(git rev-parse HEAD)
 git pull
+NEW_HEAD=$(git rev-parse HEAD)
 
-
+if [[ $OLD_HEAD != $NEW_HEAD ]]; then
+    echo "New installer found. Restarting process..."
+    exec "./install.sh $1"
+fi
 
 echo "Installation completed."
