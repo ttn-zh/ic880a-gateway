350c019b92ba9eca946d83508106c11215575142
Gonzalo Casas <gonzalo.casas@local.ch>
2015-11-22 15:59:40 +0100

    Check sudo/root permissions

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # Stop on the first sign of trouble
 set -e
 
+if [ $UID != 0 ]; then
+    echo "Operation not permitted."
+    exit 1
+fi
+
 VERSION="master"
 if [[ $1 != "" ]]; then VERSION=$1; fi
 
@@ -26,7 +31,7 @@ CURRENT_HOSTNAME=$(hostname)
 NEW_HOSTNAME="ttn-gateway"
 
 if [[ $NEW_HOSTNAME != $CURRENT_HOSTNAME ]]; then
-    echo "Updating hostname to '$NEW_HOSTNAME'"
+    echo "Updating hostname to '$NEW_HOSTNAME'..."
     hostname $NEW_HOSTNAME
     echo $NEW_HOSTNAME > /etc/hostname
     sed -i "s/$CURRENT_HOSTNAME/$NEW_HOSTNAME/" /etc/hosts
