49c6c4f9d06c8545c636414d6aa80b72313102f4
Gonzalo Casas <gonzalo.casas@local.ch>
2015-11-22 15:36:46 +0100

    Add hostname configuration step

--- install.sh
+++ install.sh
@@ -21,4 +21,15 @@ if [[ $OLD_HEAD != $NEW_HEAD ]]; then
     exec "./install.sh" "$VERSION"
 fi
 
+# Change hostname if needed
+CURRENT_HOSTNAME=$(hostname)
+NEW_HOSTNAME="ttn-gateway"
+
+if [[ $NEW_HOSTNAME != $CURRENT_HOSTNAME ]]; then
+    echo "Updating hostname to '$NEW_HOSTNAME'"
+    hostname $NEW_HOSTNAME
+    echo $NEW_HOSTNAME > /etc/hostname
+    sed -i "s/$CURRENT_HOSTNAME/$NEW_HOSTNAME/" /etc/hosts
+fi
+
 echo "Installation completed."
