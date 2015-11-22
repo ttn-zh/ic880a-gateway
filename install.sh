ad8b8f6b8a72cc4d74a22ce00217421b528f159b
Gonzalo Casas <gonzalo.casas@local.ch>
2015-11-22 14:22:36 +0100

    Initial installer

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Stop on the first sign of trouble
+set -e
+
+VERSION="master"
+if [[ $1 != ""]]; then VERSION=$1; fi
+
+# Update the gateway installer to the correct branch (defaults to master)
+git checkout -q $VERSION
+git pull
+
