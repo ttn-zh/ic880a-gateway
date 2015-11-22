c9e689a463d697ee0cd9a073e1106d06013dc7ad
Gonzalo Casas <gonzalo.casas@local.ch>
2015-11-22 15:19:50 +0100

    Add comments

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ VERSION="master"
 if [[ $1 != "" ]]; then VERSION=$1; fi
 
 # Update the gateway installer to the correct branch (defaults to master)
+echo "Updating installer files..."
 git checkout -q $VERSION
 git pull
 
+
+
+echo "Installation completed."
