51671a9b9f847293f85088cc9eff744f57a150df
Gonzalo Casas <gonzalo.casas@local.ch>
2015-12-27 21:09:30 +0100

    Reset code

--- start.sh
+++ start.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 
+# Reset iC880a PIN
+gpio -1 mode 22 out
+gpio -1 write 22 0
+sleep 0.1
+gpio -1 write 22 1
+sleep 0.1
+gpio -1 write 22 0
+sleep 0.1
+
 # Test the connection, wait if needed.
 while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
   echo "[TTN Gateway]: Waiting for internet connection..."
   sleep 30
   done
 
-# Fire up the forwarder.  
+# Fire up the forwarder.
 ./poly_pkt_fwd
