dc97d2d31807450109d3b8ff94233a623ea3d2be
Gonzalo Casas <gonzalo.casas@local.ch>
2015-12-27 21:41:37 +0100

    Add altitude

--- install.sh
+++ install.sh
@@ -44,6 +44,10 @@ printf "       Longitude [0]: "
 read GATEWAY_LON
 if [[ $GATEWAY_LON == "" ]]; then GATEWAY_LON=0; fi
 
+printf "       Altitude [0]: "
+read GATEWAY_ALT
+if [[ $GATEWAY_ALT == "" ]]; then GATEWAY_ALT=0; fi
+
 
 # Change hostname if needed
 CURRENT_HOSTNAME=$(hostname)
@@ -122,7 +126,7 @@ if [ -f ./bin/poly_pkt_fwd ]; then rm ./bin/poly_pkt_fwd; fi
 ln -s $INSTALL_DIR/packet_forwarder/poly_pkt_fwd/poly_pkt_fwd ./bin/poly_pkt_fwd
 cp -f ./packet_forwarder/poly_pkt_fwd/global_conf.json ./bin/global_conf.json
 
-echo -e "{\n\t\"gateway_conf\": {\n\t\t\"gateway_ID\": \"0000000000000000\",\n\t\t\"servers\": [ { \"server_address\": \"croft.thethings.girovito.nl\", \"serv_port_up\": 1700, \"serv_port_down\": 1701, \"serv_enabled\": true } ],\n\t\t\"ref_latitude\": $GATEWAY_LAT,\n\t\t\"ref_longitude\": $GATEWAY_LON,\n\t\t\"contact_email\": \"$GATEWAY_EMAIL\",\n\t\t\"description\": \"$GATEWAY_NAME\" \n\t}\n}" >./bin/local_conf.json
+echo -e "{\n\t\"gateway_conf\": {\n\t\t\"gateway_ID\": \"0000000000000000\",\n\t\t\"servers\": [ { \"server_address\": \"croft.thethings.girovito.nl\", \"serv_port_up\": 1700, \"serv_port_down\": 1701, \"serv_enabled\": true } ],\n\t\t\"ref_latitude\": $GATEWAY_LAT,\n\t\t\"ref_longitude\": $GATEWAY_LON,\n\t\t\"ref_altitude\": $GATEWAY_ALT,\n\t\t\"contact_email\": \"$GATEWAY_EMAIL\",\n\t\t\"description\": \"$GATEWAY_NAME\" \n\t}\n}" >./bin/local_conf.json
 
 # Reset gateway ID based on MAC
 ./packet_forwarder/reset_pkt_fwd.sh start ./bin/local_conf.json
