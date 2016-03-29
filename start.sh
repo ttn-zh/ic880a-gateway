833a577a7b40df17720e571f7020471e6c99ebcc
Gonzalo Casas <gonzalo.casas@local.ch>
2016-03-29 04:53:50 +0200

    Refresh configuration on start

--- start.sh
+++ start.sh
@@ -15,5 +15,37 @@ while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
   sleep 30
   done
 
+# If there's a remote config, try to update it
+if [ -d ../gateway-remote-config ]; then
+    # First pull from the repo
+    pushd ../gateway-remote-config/
+    git pull
+    git reset --hard
+    popd
+
+    # And then try to refresh the gateway EUI and re-link local_conf.json
+
+    # Same eth0/wlan0 fallback as on install.sh
+    GATEWAY_EUI_NIC="eth0"
+    if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
+        GATEWAY_EUI_NIC="wlan0"
+    fi
+
+    if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
+        echo "ERROR: No network interface found. Cannot set gateway ID."
+        exit 1
+    fi
+
+    GATEWAY_EUI="FFFE"$(ip link show $GATEWAY_EUI_NIC | awk '/ether/ {print $2}' | awk -F\: '{print $1$2$3$4$5$6}')
+    GATEWAY_EUI=${GATEWAY_EUI^^} # toupper
+
+    INSTALL_DIR="/opt/ttn-gateway"
+    LOCAL_CONFIG_FILE=$INSTALL_DIR/bin/local_conf.json
+
+    if [ -e $LOCAL_CONFIG_FILE ]; then rm $LOCAL_CONFIG_FILE; fi;
+    ln -s $INSTALL_DIR/gateway-remote-config/$GATEWAY_EUI.json $LOCAL_CONFIG_FILE
+
+fi
+
 # Fire up the forwarder.
 ./poly_pkt_fwd
