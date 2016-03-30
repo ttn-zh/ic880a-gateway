13ad330901cb6182bb8fa7ef5029792635aa16ae
Alexandre Bouillot <githug@bouillot.org>
2016-03-30 17:28:52 +0200

    Change GW EUI 64 generation
    According to IEEE recommendations, the FFFE should be inserted between the OUI and the 'serial' part

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
     exit 1
 fi
 
-GATEWAY_EUI="FFFE"$(ip link show $GATEWAY_EUI_NIC | awk '/ether/ {print $2}' | awk -F\: '{print $1$2$3$4$5$6}')
+GATEWAY_EUI=$(ip link show $GATEWAY_EUI_NIC | awk '/ether/ {print $2}' | awk -F\: '{print $1$2$3"FFFE"$4$5$6}')
 GATEWAY_EUI=${GATEWAY_EUI^^} # toupper
 
 echo "Detected EUI $GATEWAY_EUI from $GATEWAY_EUI_NIC"
