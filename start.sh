fc29ac6c312ece6cbc3709103ae782994d75e64c
Alexandre Bouillot <githug@bouillot.org>
2016-03-31 09:40:26 +0200

    Change Reset PIN name
    Change the pin variable name 
    added a sleep step

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
 # Reset iC880a PIN
-pin=25
-echo "$pin"  > /sys/class/gpio/export 
-echo "out" > /sys/class/gpio/gpio$pin/direction 
-echo "0"   > /sys/class/gpio/gpio$pin/value 
+SX1301_RESET_BCM_PIN=25
+echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export 
+echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction 
+echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value 
 sleep 0.1  
-echo "1"   > /sys/class/gpio/gpio$pin/value 
+echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value 
 sleep 0.1  
-echo "0"   > /sys/class/gpio/gpio$pin/value
-echo "$pin"  > /sys/class/gpio/unexport 
+echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+sleep 0.1
+echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/unexport 
 
 # Test the connection, wait if needed.
 while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
