cbce97adf73b05cecc6d0d7982b525dec21dfbfb
Gonzalo Casas <gonzalo.casas@local.ch>
2016-07-01 08:41:37 +0200

    Increase sleep time during reset just to be sure

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ SX1301_RESET_BCM_PIN=25
 echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export 
 echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction 
 echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value 
-sleep 0.1  
+sleep 0.2
 echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value 
-sleep 0.1  
+sleep 0.2
 echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
+sleep 0.2
 echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/unexport 
 
 # Test the connection, wait if needed.
