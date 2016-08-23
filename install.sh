5bffd90e592a4c9dc50d227933cc0da6106525c4
Gonzalo Casas <gonzalo.casas@local.ch>
2016-08-24 00:38:55 +0200

    Use experimental version of the lora gateway library as well

--- install.sh
+++ install.sh
@@ -106,7 +106,7 @@ fi
 
 # Build LoRa gateway app
 if [ ! -d lora_gateway ]; then
-    git clone https://github.com/TheThingsNetwork/lora_gateway.git
+    git clone https://github.com/devlaam/lora_gateway.git
     pushd lora_gateway
 else
     pushd lora_gateway
