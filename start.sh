e20144a7829bcbecd030d56f474b8826fff114fb
Lars-Magnus Skog <ralphtheninja@riseup.net>
2017-05-02 12:50:14 +0200

    remove ping test, not a guarantee for internet connectivity

--- start.sh
+++ start.sh
@@ -12,12 +12,6 @@ echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 0.1
 echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/unexport 
 
-# Test the connection, wait if needed.
-while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
-  echo "[TTN Gateway]: Waiting for internet connection..."
-  sleep 30
-  done
-
 # If there's a remote config, try to update it
 if [ -d ../gateway-remote-config ]; then
     # First pull from the repo
